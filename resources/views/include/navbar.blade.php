ccff260e2bb9d7dafce75e8bae58b7584b4b4324
talhaumer61 <talhaumer197@gmail.com>
2025-01-21 16:33:39 +0500

    Fixed  Navbar

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -29,7 +29,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link active">
+                            <a href="#" class="nav-link">
                                 Our Services 
                             </a>
                         </li>
